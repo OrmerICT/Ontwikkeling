d3bd38b72e682c28749d4d6f9ff42e6efa244e53
Ormer ICT <managed_services@ormer.nl>
2015-07-09 10:12:37 +0200

    Test commit

---
+++ Convert-OrmLogToString.ps1
@@ -0,0 +1,29 @@
+﻿function Convert-OrmLogToString {
+  [cmdletbinding()]
+  param (
+    [parameter(
+        Mandatory=$true,
+        ValueFromPipeline=$true
+    )]
+    $Object
+  )
+  begin
+  {
+  }
+  process
+  {
+    try
+    {
+      '[{0}][{1}][{2}][{3}][{4}][{5}][{6}][{7}]' -f $Object.Date,$Object.Time,$Object.Output,$Object.IdentityDomain,$Object.IdentityUserName,$Object.Status,$Object.Action,$Object.Message
+    }
+    catch
+    {
+      Write-Error $_
+    }
+  }
+  end
+  {
+  }
+}
+
+Convert-OrmLogToString
