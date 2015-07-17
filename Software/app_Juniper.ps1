d274cee1a7ae7359621e3285b80611ee662a9e4e
Raymond de Wit <r.wit@ormer.nl>
2015-07-17 13:55:02 +0200

    Start

---
+++ Software/app_Juniper.ps1
@@ -0,0 +1,55 @@
+﻿[cmdletbinding()]
+param (
+    [parameter(mandatory=$false)]
+    [string]$Operator,
+
+    [parameter(mandatory=$false)]
+    [string]$MachineGroep,
+
+    [parameter(mandatory=$false)]
+    [string]$TDNumber,
+
+    [parameter(mandatory=$true)]
+    [string]$KworkingDir
+)
+
+Set-Location $KworkingDir
+    
+. .\WriteLog.ps1
+$Domain = $env:USERDOMAIN
+$MachineName = $env:COMPUTERNAME
+$GetProcName = Get-PSCallStack
+$procname = $GetProcname.Command
+$Customer = $MachineGroep.Split(“.”)[2]
+
+#region Object
+$logvar = New-Object -TypeName PSObject -Property @{
+    'Domain' = $Domain 
+    'MachineName' = $MachineName
+    'procname' = $procname
+    'Customer' = $Customer
+    'Operator'= $Operator
+    'TDNumber'= $TDNumber
+}
+#endregion Object
+    
+#region Execution
+f_New-Log -logvar $logvar -status 'Start' -Message "START $procname" -LogDir $KworkingDir
+
+## Check if app already installed
+$CheckApp = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object{$_. Displayname -Like "Juniper Networks Setup Client Activex Control"}
+    if ($CheckApp) {
+        f_New-Log -logvar $logvar -status 'Info' -Message 'JuniperSetupClientInstaller.exe already installed' -LogDir $KworkingDir
+        }
+    else {
+        f_New-Log -logvar $logvar -status 'Info' -Message 'Juniper Networks Setup Client Activex Control not found' -LogDir $KworkingDir
+        Start-Process -FilePath ".\JuniperSetupClientInstaller\JuniperSetupClientOCX.exe" -WorkingDirectory $KworkingDir -Wait
+        Start-Process -FilePath ".\JuniperSetupClientInstaller\JuniperSetupClientOCX64.exe" -Wait
+        f_New-Log -logvar $logvar -status 'Info' -Message 'Google Chrome installed successfully' -LogDir $KworkingDir
+        }
+    
+f_New-Log -logvar $logvar -status 'Info' -Message "Remove JuniperSetupClientInstaller install directory" -LogDir $KworkingDir
+Remove-Item “.\JuniperSetupClientInstaller” -recurse -force
+f_New-Log -logvar $logvar -status 'Success' -Message "END $procname" -LogDir $KworkingDir
+
+#endregion Execution
