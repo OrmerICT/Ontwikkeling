5efc1b4fb41f86b74a56a4afe6b70a8cf6ba063a
Ormer ICT <managed_services@ormer.nl>
2015-07-20 13:00:59 +0200

    Logging Scripts Aangepast
    Logging Scripts Aangepast

---
+++ AD/DelTemp.ps1
@@ -0,0 +1,221 @@
+﻿###############################################################################
+#   Ormer LEGAL STATEMENT FOR SAMPLE SCRIPTS/CODE
+###############################################################################
+
+#******************************************************************************
+# File:     Deltemp.ps1
+# Date:     07/14/2015
+# Version:  1.0
+#
+# Purpose:  PowerShell script to clean Temp folders.
+#
+# Usage:    Deltemp -help | -path "<Font file or folder path>"
+#
+# Copyright (C) 2015 Ormer ICT  By PvdW
+#
+# https://social.technet.microsoft.com/Forums/en-US/159e6c66-a8e4-40a5-80b0-c43f4837bcd6/deleting-a-folder-in-all-users-profiles?forum=ITCG
+# 
+#
+# Revisions:
+# ----------
+# 1.0.0   07/15/2015   Created script.
+# 
+#******************************************************************************
+
+#*******************************************************************
+# Declare Parameters
+#*******************************************************************
+
+#*******************************************************************
+# Declare Global Variables and Constants
+#*******************************************************************
+
+# Define constants
+
+# Initialize variables
+
+#   Test Settings   ###
+#$KworkingDir = "c:\kworking\"
+#$LogDir = "c:\kworking\"
+#$MachineGroep = "MG_01"
+#$Operator = "Operator-Ducje"
+#$procname  = "ProcesName-DelTmp"
+#$TDNumber  = "TD12345"
+#$Customer  = "CU-TestDucje"
+#$Domain0 = "DO-Ducje"
+
+#*******************************************************************
+#  Load C# code
+#*******************************************************************
+
+#*******************************************************************
+# Declare Functions
+#*******************************************************************
+function f_Logging-Module {
+[cmdletbinding()]
+param (
+    [parameter(mandatory=$false)]
+    [string]$Operator,
+
+    [parameter(mandatory=$false)]
+    [string]$MachineGroep,
+
+    [parameter(mandatory=$false)]
+    [string]$TDNumber,
+
+    [parameter(mandatory=$false)]
+    [string]$KworkingDir,
+
+    # Procedure vars
+    [Parameter(Mandatory=$false)]
+                [String] $UserName,
+                
+                [Parameter(Mandatory=$false)]
+                [String] $PassWord,
+
+                [Parameter(Mandatory=$false)]
+                [String] $Domain = $env:USERDOMAIN
+)
+}
+
+Set-Location $KworkingDir
+    
+. .\WriteLog.ps1
+ $Domain = $Domain0
+#t $Domain = $env:USERDOMAIN
+$MachineName = $env:COMPUTERNAME
+$procname = $MyInvocation.Scriptname.Split(“\”)[2]
+#t $Customer = $MachineGroep.Split(“.”)[2]
+
+#region Object
+$logvar = New-Object -TypeName PSObject -Property @{
+    'Domain' = $Domain 
+    'MachineName' = $MachineName
+    'procname' = $procname
+    'Customer' = $Customer
+    'Operator'= $Operator
+    'TDNumber'= $TDNumber
+}
+#endregion Object
+
+#*******************************************************************
+# Function Clean1 Temp Folder -1
+#
+#*******************************************************************
+function f_Clean1-Module{
+f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message "Loop"
+   Get-ChildItem -Path $SearchRoot | ForEach-Object {
+        if ($_.PSIsContainer -eq ,$true) {
+           if ((Get-ChildItem -Path $_.FullName) -eq $null) {
+            Write-Host "$($_.FullName) is empty."
+            f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Check: `'$($_.FullName)`' Is empty"
+                       
+        } else {
+            Write-Host "$($_.FullName) is not empty."
+            Remove-Item $($_.FullName) -recurse -ErrorAction SilentlyContinue
+            f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Remove`'$($_.FullName)`' folder"
+            }
+    }
+  } 
+}
+
+   #End of Function f_Clean1-Module
+##########################################################################################################
+
+#*******************************************************************
+# Function Clean2012 Clean User Profile Temp folders
+#
+#*******************************************************************
+function f_Clean_Users-2012-Module{
+
+foreach ($user in $users){
+$folder = "C:\Users\" + $user + $SubPath
+#Write-Host $folder, $user, $users
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Remove `'$($folder)`' temp files"
+Remove-Item $folder\* -Recurse -Force -ErrorAction silentlycontinue #-WhatIf
+}
+}                 
+   #End of Function f_Clean_Users-Module
+##########################################################################################################
+
+#*******************************************************************
+# Function Clean2003 Clean User Profile Temp folders
+#
+#*******************************************************************
+function f_Clean_Users-2003-Module{
+
+foreach ($user in $users){
+$folder = "C:\Documents and Settings\Users\" + $user + $SubPath
+Write-Host $folder, $user
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Remove `'$($folder)`' temp files"
+Remove-Item $folder\* -Recurse -Force -ErrorAction silentlycontinue -WhatIf
+}
+}                 
+   #End of Function f_Clean_Users-Module
+##########################################################################################################
+
+#*******************************************************************
+# Main Script
+#*******************************************************************
+Set-ExecutionPolicy unrestricted
+
+# Write $LogDir, $Customer, $KworkingDir
+remove-item "C:\kworking\ProcedureLog.log" -Force
+    
+#region Execution
+f_Logging-Module
+f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message $procname
+f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message "Clean Temp folders Script"
+# f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Check: `'$($KworkingDir)`' Working Folder"
+
+# Clean local Temp folder 
+$SearchRoot = "C:\temp"
+f_Clean1-Module
+#$users = Get-ChildItem "C:\"
+#$SubPath = "temp"
+#f_Clean_Users-2012-Module
+
+# Clean User Profile Temp folders
+$users = Get-ChildItem "C:\Users"
+$SubPath = "\AppData\Local\temp"
+f_Clean_Users-2012-Module
+
+# Clean User (2008) Internet Temp folders
+$users = Get-ChildItem "C:\Users"
+$SubPath = "\local settings\Temporary Internet Files"
+f_Clean_Users-2012-Module
+
+# Clean User (2008) Temp folders
+$users = Get-ChildItem "C:\Users"
+$SubPath = "\local settings\Temp"
+f_Clean_Users-2012-Module
+
+# Clean User (2012) Internet Temp folders
+$SubPath = "\AppData\Local\Microsoft\Windows\INetCache"
+f_Clean_Users-2012-Module 
+
+# Clean User (2012) Internet Temp folders
+$users = Get-ChildItem "C:\Users"
+$SubPath = "\local settings\Microsoft\Windows\Temporary Internet Files"
+f_Clean_Users-2012-Module
+
+# Clean User Dropbox Temp folders
+$SubPath = "\Dropbox\.Dropbox.Cache"
+f_Clean_Users-2012-Module 
+
+# Clean (2003) User Internet Temp folders
+$users = Get-ChildItem "C:\Documents and Settings\Users"
+$SubPath = "\Local Settings\Temporary Internet Files"
+f_Clean_Users-2003-Module
+
+# Clean (2003) User Temp folders
+$users = Get-ChildItem "C:\Documents and Settings\Users"
+$SubPath = "\Local Settings\Temp"
+f_Clean_Users-2003-Module
+
+ 
+#endregion Execution
+
+
+
+
---
+++ AD/Unlock-AdUser-v1.2.ps1
@@ -0,0 +1,203 @@
+﻿###############################################################################
+#   Ormer LEGAL STATEMENT FOR SAMPLE SCRIPTS/CODE
+<###############################################################################
+
+#******************************************************************************
+# File:     Unlock-AdUser-v1.1.ps1
+# Date:     07/16/2015
+# Version:  1.0
+#
+# Purpose:  PowerShell script to clean Temp folders.
+#
+# Usage:    Unlock-AdUser-v1.1.ps1
+# Needed: Remote administration tools to load the server manager
+#
+# Copyright (C) 2015 Ormer ICT 
+# http://www.informit.com/articles/article.aspx?p=729101&seqNum=5
+# http://bobausmus.blogspot.nl/2013/10/powershell-module-and-snapin-checks.html
+#
+# Revisions:
+# ----------
+# 1.0.0   07/16/2015   Created script.
+# 1.1.0   07/17/2015   Error logging aangepast By PvdW
+# 1.2.0   07/17/2015   Error logging aangepast By PvdW
+#>#******************************************************************************
+
+#*******************************************************************
+# Declare Parameters
+#*******************************************************************
+
+#*******************************************************************
+# Declare Global Variables and Constants
+#*******************************************************************
+
+# Define constants
+
+# Initialize variables
+
+#   Test Settings   ###
+$KworkingDir = "c:\kworking\"
+#$LogDir = "c:\kworking\"
+$MachineGroep = "MG_01.mg"
+#$Operator = "Operator-Ducje"
+#$procname  = "ProcesName-DelTmp"
+#$TDNumber  = "TD12345"
+$Customer  = "CU-TestDucje"
+#$Domain0 = "DO-Ducje"
+#$UserName = "Test-Ducje"
+#$Title = "Unlock AD User"
+
+#*******************************************************************
+#  Load C# code
+#*******************************************************************
+
+#*******************************************************************
+# Declare Functions
+#*******************************************************************
+function f_Logging-Module {
+[cmdletbinding()]
+param (
+    [parameter(mandatory=$false)]
+    [string]$Operator,
+
+    [parameter(mandatory=$false)]
+    [string]$MachineGroep,
+
+    [parameter(mandatory=$false)]
+    [string]$TDNumber,
+
+    [parameter(mandatory=$false)]
+    [string]$KworkingDir,
+	
+	# Procedure vars
+    [Parameter(Mandatory=$false)]
+                [String] $UserName,
+                
+                [Parameter(Mandatory=$false)]
+                [String] $PassWord,
+
+                [Parameter(Mandatory=$false)]
+                [String] $Domain = $env:USERDOMAIN
+)
+}
+#write-host $KworkingDir
+Set-Location $KworkingDir
+    
+. .\WriteLog.ps1
+$Domain = $Domain0
+$Domain = $env:USERDOMAIN
+$MachineName = $env:COMPUTERNAME
+$procname = $MyInvocation.Scriptname.Split(“\”)[2]
+$Customer = $MachineGroep.Split(“.”)[2]
+
+#region Object
+$logvar = New-Object -TypeName PSObject -Property @{
+    'Domain' = $Domain 
+    'MachineName' = $MachineName
+    'procname' = $procname
+    'Customer' = $Customer
+    'Operator'= $Operator
+    'TDNumber'= $TDNumber
+}
+#endregion Object
+
+#*******************************************************************
+# Function Clean1 Temp Folder -1
+#
+#*******************************************************************
+
+   #End of Function f_Clean1-Module
+##########################################################################################################
+
+#*******************************************************************
+# Function Clean2012 Clean User Profile Temp folders
+#
+#*******************************************************************
+   #End of Function f_Clean_Users-Module
+##########################################################################################################
+
+#*******************************************************************
+# Function Clean2003 Clean User Profile Temp folders
+#
+#*******************************************************************
+   #End of Function f_Clean_Users-Module
+##########################################################################################################
+
+#*******************************************************************
+# Main Script
+#*******************************************************************
+Set-ExecutionPolicy unrestricted
+
+# Write $LogDir, $Customer, $KworkingDir
+remove-item "C:\kworking\ProcedureLog.log" -Force -ErrorAction SilentlyContinue
+    
+#region Execution
+f_Logging-Module
+f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message "Unlock AS User Account Script"
+# f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Title: `'$($KworkingDir)`' Script"
+
+# Load module Server manager
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Checking to see if the servermanager PowerShell module is installed"
+if ((get-module -name servermanager -ErrorAction SilentlyContinue | foreach { $_.Name }) -ne "servermanager")
+{
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Adding servermanager PowerShell module" 
+import-module servermanager
+}
+else
+{
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "servermanager PowerShell module is Already loaded" 
+}
+ 
+# Install RSAT-AD-PowerShell
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Check if RSAT-AD-PowerShell is installed"
+$RSAT = (Get-WindowsFeature -name RSAT-AD-PowerShell).Installed 
+
+If ($RSAT -eq $false)
+{
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "RSAT-AD-PowerShell not found: `'$($RSAT)`'"
+
+Add-WindowsFeature RSAT-AD-PowerShell
+f_New-Log -logvar $logvar -status 'start' -LogDir $KworkingDir -Message "Add Windows Feature RSAT-AD-PowerShell"
+
+Import-module ActiveDirectory
+f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message "Import module ActiveDirectory"
+}
+Else
+{
+f_New-Log -logvar $logvar -status 'start' -LogDir $KworkingDir -Message "Windows Feature RSAT-AD-PowerShell installed"
+}
+
+#Import Module Active Directory
+if ((get-module -name ActiveDirectory -ErrorAction SilentlyContinue | foreach { $_.Name }) -ne "ActiveDirectory")
+{
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Adding ActiveDirectory PowerShell module" 
+import-module ActiveDirectory
+}
+else
+{
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "ActiveDirectory PowerShell module is Already loaded" 
+}
+
+#Check if user is disabled
+$UserEnabled = (Get-ADUser -Identity $UserName).Enabled
+If ($UserEnabled -eq $false)
+{
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "User is disabled. Please contact the manager: `'$($UserEnabled)`'"
+exit
+}
+else
+{
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "User is enabled: `'$($UserName)`'"
+}
+
+#Unlock Account
+f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "Unlock user account: `'$($UserName)`'"
+Unlock-ADAccount -Identity $UserName
+
+
+ 
+#endregion Execution
+
+
+
+
