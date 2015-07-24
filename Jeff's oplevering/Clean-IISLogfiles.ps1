06275e1bfcb55a24f4342bc4bdc9d80096b1227a
Jeff Wouters <contact@jeffwouters.nl>
2015-07-24 10:56:36 +0200

    Clean-IISLogFiles & Get-IISLogDirectory
    Clean-IISLogFiles & Get-IISLogDirectory voor Bas.

---
+++ Jeff's oplevering/Clean-IISLogfiles.ps1
@@ -0,0 +1,44 @@
+﻿function Clean-IISLogfiles
+{
+<#
+  .SYNOPSIS
+  Clean the IIS log files.
+  .DESCRIPTION
+  Clean the IIS log files older than XX days, where the default is 31 days.
+  .EXAMPLE
+  Clean-IISLogFiles -Path 'C:\inetpub\logs\LogFiles\W3SVC1' -Days '10'
+  .EXAMPLE
+  Clean-IISLogFiles 'C:\inetpub\logs\LogFiles\W3SVC1' -Days '10'
+  .EXAMPLE
+  Clean-IISLogFiles 'C:\inetpub\logs\LogFiles\W3SVC1'
+  .NOTES
+  Version  :  0.1
+  Customer :  Ormer ICT
+  Author   :  Jeff Wouters
+#>
+  param (
+    [parameter(
+        Mandatory=$false,
+        Position=0
+    )]$Path = 'C:\inetpub\logs\LogFiles\W3SVC1',
+    [parameter(
+        Mandatory=$false
+    )]$Days = 31
+  )
+  foreach ($File in (Get-ChildItem -Path $Path))
+  {
+    if (!$File.PSIsContainerCopy) 
+    {
+      if ($File.LastWriteTime -lt ($(Get-Date).Adddays(-$days))) 
+      {
+        try
+        {
+          Remove-Item -Path $File -Force
+          Write-Verbose "Removed logfile $File"
+        } catch {
+          Write-Verbose "Error: Unable to removed logfile $File"
+        }
+      }
+    }
+  } 
+}
---
+++ Jeff's oplevering/Get-IISLogDirectory.ps1
@@ -0,0 +1,22 @@
+﻿function Get-IISLogDirectory
+{
+  <#
+      .SYNOPSIS
+      Get the IIS log directory.
+      .DESCRIPTION
+      Get the IIS log directory.
+      .NOTES
+      Version  :  0.1
+      Customer :  Ormer ICT
+      Author   :  Jeff Wouters
+  #>
+  [cmdletbinding()]
+  param ()
+  try {
+    Import-Module WebAdministration
+    (Get-WebConfigurationProperty '/system.applicationHost/sites/siteDefaults' -Name 'logfile.directory').Value
+  }
+  catch {
+    Write-Error $_
+  }
+}
