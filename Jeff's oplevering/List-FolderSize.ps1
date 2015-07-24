9f4dc0e07fe5d03781503c8d42cd7008c3415be4
Jeff Wouters <contact@jeffwouters.nl>
2015-07-24 12:04:57 +0200

    List-FolderSize converted to framework template
    List-FolderSize converted to framework template

--- Jeff's oplevering/List-FolderSize.ps1
+++ Jeff's oplevering/List-FolderSize.ps1
@@ -1,21 +1,17 @@
-﻿Function List-FolderSize { 
-<#
-  .SYNOPSIS
-  List the path and size of folders.
-  .DESCRIPTION
-  List the path and size of folders to a definable depth in MB, GB or TB up to 
-  three decimals.
-  .EXAMPLE
-  Get-ChildItemToDepth -Path 'D:\' -ToDepth 3 -Format GB -ErrorAction SilentlyContinue | 
-    Sort-Object -Property size | 
-      ConvertTo-Csv | 
-        Out-File -FilePath c:\kworking\FolderSize.csv
-  .NOTES
-  Version  :  0.1
-  Customer :  Ormer ICT
-  Author   :  Jeff Wouters
-#>
-  Param( 
+﻿[cmdletbinding()]
+param (
+    [parameter(mandatory=$false)]
+    [string]$Operator,
+
+    [parameter(mandatory=$false)]
+    [string]$MachineGroep,
+
+    [parameter(mandatory=$false)]
+    [string]$TDNumber,
+
+    [parameter(mandatory=$true)]
+    [string]$KworkingDir,
+
     [parameter(
         Mandatory=$true,
         Position=0
@@ -34,21 +30,57 @@
         Mandatory=$false
     )]
     [Byte]$CurrentDepth = 0
-  ) 
-  
+)
+
+#region StandardFramework
+Set-Location $KworkingDir
+    
+. .\WriteLog.ps1
+$Domain = $env:USERDOMAIN
+$MachineName = $env:COMPUTERNAME
+$GetProcName = Get-PSCallStack
+$procname = $GetProcname.Command
+$Customer = $MachineGroep.Split(“.”)[2]
+
+$logvar = New-Object -TypeName PSObject -Property @{
+    'Domain' = $Domain 
+    'MachineName' = $MachineName
+    'procname' = $procname
+    'Customer' = $Customer
+    'Operator'= $Operator
+    'TDNumber'= $TDNumber
+}
+
+Remove-Item "$KworkingDir\ProcedureLog.log" -Force -ErrorAction SilentlyContinue
+f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message "Title: `'$Kworking`' Script"
+#endregion StandardFramework
+    
+#region Execution
   $CurrentDepth++
   Get-ChildItem $Path | Where-Object {$_.Attributes -eq 'Directory' -and $_.FullName.length -lt 260 } | foreach {
-    $itemSum = Get-ChildItem $_.FullName -recurse | Where-Object {$_.length -gt 0 } | Measure-Object -property length -sum     
-    New-Object -TypeName PSObject -Property @{
-      'FullName' = $_.FullName
-      'Size' = '{0:N3}' -f ($itemSum.sum / ('1'+$Format))
+    f_New-Log -logvar $logvar -status 'Info' -Message "Enumerating $($_.FullName)" -LogDir $KworkingDir
+    try {
+      $itemSum = Get-ChildItem $_.FullName -recurse | Where-Object {$_.length -gt 0 } | Measure-Object -property length -sum     
+      New-Object -TypeName PSObject -Property @{
+        'FullName' = $_.FullName
+        'Size' = '{0:N3}' -f ($itemSum.sum / ('1'+$Format))
+      }
+      f_New-Log -logvar $logvar -status 'Success' -Message "Enumerated $($_.FullName)" -LogDir $KworkingDir
+    }
+    catch
+    {
+      f_New-Log -logvar $logvar -status 'Error' -Message 'Error found calculating the sum' -LogDir $KworkingDir
     }
     If ($_.PsIsContainer) { 
       If ($CurrentDepth -le $ToDepth) {
+        Write-Verbose "$(Get-Date -f T) - Folder: $($_.FullName)"
         Get-ChildItemToDepth -Path $_.FullName -ToDepth $ToDepth -CurrentDepth $CurrentDepth
-      } Else { 
-         Write-Debug $("Skipping Folder: $($_.FullName) ")          
+      } Else {
+        f_New-Log -logvar $logvar -status 'Info' -Message "Skipping $($_.FullName)" -LogDir $KworkingDir
       } 
     } 
-  } 
-}
+  }
+
+f_New-Log -logvar $logvar -status 'Error' -Message 'Hello world' -LogDir $KworkingDir
+    
+#endregion Execution
