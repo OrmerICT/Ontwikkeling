a675de3a2783a18c9ba44c4b87be8ede1daa4203
Jeff Wouters <contact@jeffwouters.nl>
2015-07-24 10:35:37 +0200

    DISM commands for Bas
    DISM commands for Bas - For cleaning a server (goal: save disk space).

---
+++ Jeff's oplevering/DISMCommands.ps1
@@ -0,0 +1,7 @@
+﻿# DISM Commands
+# Remove ServicePack installation files - After this an ServicePack can't be uninstalled!!!
+dism /online /cleanup-image /spsuperseded /NoRestart /Quiet
+# If an OS upgrade was performed, this removes old mountpoints from this upgrade.
+dism /Cleanup-Mountpoints
+# Remove old installation files from Windows updates - After this a previously installed update can't be uninstalled!
+dism /online /cleanup-image /StartComponentCleanup
