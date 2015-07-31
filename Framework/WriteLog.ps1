fe6b34a2343a488d7a1b19a6331e903ca1187a60
Christian Dekker <managed_services@ormer.nl>
2015-07-31 11:06:42 +0200

    Modified Log function
    Logging the time and date now takes hours, minutes and seconds, days and
    months <10 into account, adding a leading '0'

--- Framework/WriteLog.ps1
+++ Framework/WriteLog.ps1
@@ -13,6 +13,56 @@
   }
 }
 
+function Format-LogDateTime
+{
+  param (
+    [datetime]$DateTime
+  )
+    if(($DateTime.Day -lt 10)){
+        $Day = "0$($DateTime.Day)"
+    }
+    else{
+        $May = "$($DateTime.Day)"
+    }
+
+    if(($DateTime.Month -lt 10)){
+        $Month = "0$($DateTime.Month)"
+    }
+    else{
+        $Month = "$($DateTime.Month)"
+    }
+
+    $Year = "$($DateTime.Year)"
+
+    if(($DateTime.Hour -lt 10)){
+        $Hour = "0$($DateTime.Hour)"
+    }
+    else{
+        $Hour = "$($DateTime.Hour)"
+    }
+
+    if(($DateTime.Minute -lt 10)){
+        $Minute = "0$($DateTime.Minute)"
+    }
+    else{
+        $Minute = "$($DateTime.Minute)"
+    }
+
+    if(($DateTime.Second -lt 10)){
+        $Second = "0$($DateTime.Second)"
+    }
+    else{
+        $Second = "$($DateTime.Second)"
+    }
+    $Millisecond = "$($DateTime.Millisecond)"
+
+    $TimeArray = @()
+    $TimeArray+=("$($Day)-$($Month)-$($Year)")
+    $TimeArray+=("$($Hour):$($Minute):$($Second):$($Millisecond)")
+
+    Return $TimeArray
+}
+
 function f_New-Log {
     [cmdletbinding()]
     param (
@@ -29,8 +79,9 @@ function f_New-Log {
     try 
     {
         $DateTime = ConvertTo-WEuropeStandardTime -DateTime (Get-Date)
-        $LogDate = "$($DateTime.Day)-$($DateTime.Month)-$($DateTime.Year)"
-        $LogTime = "$($DateTime.Hour):$($DateTime.Minute):$($DateTime.Second):$($DateTime.Millisecond)"
+        $FormattedDateTime = Format-LogDateTime -DateTime $DateTime
+        $LogDate = $FormattedDateTime[0]
+        $LogTime = $FormattedDateTime[1]
         #$Ormlogstring = '[{0}][{1}][{2}][{3}][{4}][{5}][{6}][{7}][{8}]' -f $($DateTime.SubString(0,10)),$($DateTime.SubString(11)),$logvar.Operator,$logvar.Domain,$Logvar.MachineName,$logvar.Customer,$logvar.TDNumber,$Status,$Message
         #$DateTime = Get-Date -Format 'yyyy-MM-dd hh:mm:ss'
         $Ormlogstring = '[{0}][{1}][{2}][{3}][{4}][{5}][{6}][{7}][{8}][{9}]' -f $LogDate,$LogTime,$logvar.Operator,$logvar.Domain,$Logvar.MachineName,$logvar.Customer,$logvar.TDNumber,$logvar.Procname,$Status,$Message
