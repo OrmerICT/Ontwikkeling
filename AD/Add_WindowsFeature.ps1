5d6f4f882ff886909ee334ddebb2b66f5daea298
Pwerke <P.werke@ormer.nl>
2015-07-31 09:40:48 +0200

    Updated

--- AD/Add_WindowsFeature.ps1
+++ AD/Add_WindowsFeature.ps1
@@ -5,7 +5,7 @@
 #******************************************************************************
 # File:     Add_WindowsFeature.ps1
 # Date:     07/30/2015
-# Version:  0.2
+# Version:  0.3
 #
 # Purpose:  PowerShell script to add a new Feature to a Windows server(s).
 #
@@ -18,6 +18,7 @@
 # ----------
 # 0.1.0   07/28/2015   Created script. (By PvdW)
 # 0.2.0   07/30/2015   Updated script. (By PvdW)
+# 0.3.0   07/31/2015   Updated script. Remove inputbox. Input nu via Kaseya (By PvdW)
 #    
 #>#******************************************************************************
 #region Start Parameters
@@ -60,8 +61,8 @@ Usage:
 Help:
 Add_WindowsFeature.ps1 -help 
 
-Install:
-Add_WindowsFeature.ps1 -kworking -TDnumber
+Install/Add:
+Add_WindowsFeature.ps1 -kworking -TDnumber -FeatureChoice
 
 Parameters:
 
@@ -95,156 +96,7 @@ function Process-Arguments()
     }
 }
 #endregion Function Process-Arguments
-
-#region Function Dropdown
-function Return-DropDown {
- $script:Choice = $DropDown.SelectedItem.ToString()
- $Form.Close()
- Write-Host $Choice
-}
-#endregion Function Dropdown
-
-function OnApplicationLoad {
-	#Note: This function runs before the form is created
-	#Note: To get the script directory in the Packager use: Split-Path $hostinvocation.MyCommand.path
-	#Note: To get the console output in the Packager (Windows Mode) use: $ConsoleOutput (Type: System.Collections.ArrayList)
-	#Important: Form controls cannot be accessed in this function
-	#TODO: Add snapins and custom code to validate the application load
 	
-	return $true #return true for success or false for failure
-}
-
-function OnApplicationExit {
-	#Note: This function runs after the form is closed
-	#TODO: Add custom code to clean up and unload snapins when the application exits
-	
-	$script:ExitCode = 0 #Set the exit code for the Packager
-}
-
-function GenerateForm {
-
-	#----------------------------------------------
-	#region Import Assemblies
-	#----------------------------------------------
-	[void][reflection.assembly]::Load("System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")
-	[void][reflection.assembly]::Load("System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
-	[void][reflection.assembly]::Load("mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")
-	[void][reflection.assembly]::Load("System.Data, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")
-	[void][reflection.assembly]::Load("System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")
-	#endregion
-	
-	#----------------------------------------------
-	#region Generated Form Objects
-	#----------------------------------------------
-	[System.Windows.Forms.Application]::EnableVisualStyles()
-	$form1 = New-Object System.Windows.Forms.Form
-	$label1 = New-Object System.Windows.Forms.Label
-	$button1 = New-Object System.Windows.Forms.Button
-    $button2 = New-Object System.Windows.Forms.Button
-	$combobox1 = New-Object System.Windows.Forms.ComboBox
-	$InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState
-	#endregion Generated Form Objects
-
-	#----------------------------------------------
-	# User Generated Script
-	#----------------------------------------------
-	
-	$FormEvent_Load={
-		#TODO: Initialize Form Controls here
-		#Import-Csv -Delimiter ";" -Path C:\kworking\FeaturesW2K12.txt | % {
-         Import-Csv -Path C:\kworking\FeaturesW2K12.csv | % {
-		$combobox1.Items.Add($_.name)
-		}
-	}
-	
-	$handler_button1_Click={
-	#TODO: Place custom script here
-		#$x = Import-Csv -Delimiter ";" -Path C:\kworking\FeaturesW2K12.txt | ? { $_.name -eq $combobox1.Text } | Select -first 1
-        $x = Import-Csv -Path C:\kworking\FeaturesW2K12.csv | ? { $_.name -eq $combobox1.Text } | Select -first 1
-	#	$label1.Text = (Test-Connection -ComputerName $x.server -Quiet).toString()
-        $FeatureChoice = $combobox1.Text
-        Write-Host $combobox1.Text
-        Add-WindowsFeature -Name $FeatureChoice -ErrorAction SilentlyContinue -WhatIf
-        }
-	
-	#----------------------------------------------
-	# Generated Events
-	#----------------------------------------------
-	
-	$Form_StateCorrection_Load=
-	{
-		#Correct the initial state of the form to prevent the .Net maximized form issue
-		$form1.WindowState = $InitialFormWindowState
-	}
-	
-	#region Start Form
-	# form1
-	#
-	$form1.Controls.Add($label1)
-	$form1.Controls.Add($button1)
-    $form1.Controls.Add($button2)
-	$form1.Controls.Add($combobox1)
-	$form1.ClientSize = New-Object System.Drawing.Size(900,400)
-	$form1.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
-	$form1.Name = "form1"
-	$form1.Text = "Add Windows Feature"
-	$form1.add_Load($FormEvent_Load)
-    $Form1.StartPosition = "CenterScreen"
-	#
-	# label1
-	#
-	$label1.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
-	$label1.Location = New-Object System.Drawing.Point(40,25)
-	$label1.Name = "label1"
-	$label1.Size = New-Object System.Drawing.Size(500,50)
-	$label1.TabIndex = 2
-	$label1.Text = "Add Windows Feature"
-	#
-	# button1
-	#
-	$button1.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
-	$button1.Location = New-Object System.Drawing.Point(100,250)
-	$button1.Name = "button1"
-	$button1.Size = New-Object System.Drawing.Size(200,50)
-	$button1.TabIndex = 1
-	$button1.Text = "OK"
-	$button1.UseVisualStyleBackColor = $True
-	$button1.add_Click($handler_button1_Click)
-	#
-	# button2
-	#
-    $button2.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
-	$button2.Location = New-Object System.Drawing.Point(550,250)
-	$button2.Name = "button2"
-	$button2.Size = New-Object System.Drawing.Size(200,50)
-	$button2.TabIndex = 3
-	$button2.Text = "Cancel"
-	$button2.UseVisualStyleBackColor = $True
-	$button2.Add_Click({
-    $Form1.DialogResult = "Cancel"
-    $Form1.close()
-    })
-           
-    #$Form.Controls.Add($CancelButton)
-    #
-	# combobox1
-	#
-	$combobox1.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
-	$combobox1.FormattingEnabled = $True
-	$combobox1.Location = New-Object System.Drawing.Point(40,150)
-	$combobox1.Name = "combobox1"
-	$combobox1.Size = New-Object System.Drawing.Size(800,90)
-	$combobox1.TabIndex = 0
-	#endregion Start Form
-
-	#Save the initial state of the form
-	$InitialFormWindowState = $form1.WindowState
-	#Init the OnLoad event to correct the initial state of the form
-	$form1.add_Load($Form_StateCorrection_Load)
-	#Show the Form
-	return $form1.ShowDialog()
-
-} #End Function
 
 #region StandardFramework
 Set-Location $KworkingDir
@@ -272,16 +124,9 @@ remove-item "$KworkingDir\ProcedureLog.log" -Force -ErrorAction SilentlyContinue
     f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message "Title:`'$($Procname)`'Script"
 #endregion Start log
 
-#region start Call OnApplicationLoad to initialize
-if(OnApplicationLoad -eq $true)
-{
-	#Create the form
-	GenerateForm | Out-Null
-	#Perform cleanup
-	OnApplicationExit
-}
-#endregion OnApplicationLoad to initialize
 
+ Add-WindowsFeature -Name $FeatureChoice -ErrorAction SilentlyContinue
+ f_New-Log -logvar $logvar -status 'Start' -LogDir $KworkingDir -Message "Add Feature:`'$($FeatureChoice)`'"
 
 #region end log
         f_New-Log -logvar $logvar -status 'Info' -LogDir $KworkingDir -Message "END Title:`'$($Procname)`'Script"
