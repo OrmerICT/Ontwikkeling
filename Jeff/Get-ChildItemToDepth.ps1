245bba2a93b43c9a8f98688b37e187f01a29b4df
Jeff Wouters <contact@jeffwouters.nl>
2015-08-21 12:02:41 +0200

    Get-ChildItemToDepth
    Get-ChildItemToDepth

---
+++ Jeff/Get-ChildItemToDepth.ps1
@@ -0,0 +1,27 @@
+﻿function Get-ChildItemToDepth
+{
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    $Path,
+    
+    $Filter = '*',
+    
+    [System.Int32]
+    $ToDepth = 3,
+    
+    [System.Int32]
+    $CurrentDepth = 0
+  )
+  
+  $CurrentDepth++
+
+  Get-ChildItem -Path $Path -Filter $Filter -File
+  
+  if ($CurrentDepth -le $ToDepth)
+  {
+    Get-ChildItem -Path $Path -Directory |
+      ForEach-Object { Get-ChildItemToDepth -Path $_.FullName -Filter $Filter -CurrentDepth $CurrentDepth -ToDepth $ToDepth}
+  } 
+}
+# Get-ChildItemToDepth -Path 'D:\Vuze Downloads' -ToDepth 5 -CurrentDepth 2
