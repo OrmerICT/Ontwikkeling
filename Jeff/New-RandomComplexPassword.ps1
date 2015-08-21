07aa5e5508454ba0c1134c27c6dc84b281850017
Jeff Wouters <contact@jeffwouters.nl>
2015-08-21 12:03:02 +0200

    New-RandomComplexPassword
    New-RandomComplexPassword

--- Jeff/New-RandomComplexPassword.ps1
+++ Jeff/New-RandomComplexPassword.ps1
@@ -1,26 +1,13 @@
-﻿Function New-RandomComplexPassword ()
+﻿Function New-RandomComplexPassword
 {
-  <#
-      .SYNOPSIS
-      Generates a random password.
-      .DESCRIPTION
-      Generates a random password that complies with the password complexibility rules from Windows.
-      The length of the password can be configured via the Length parameter.
-      .PARAMETER Length
-      To define the length of the password.
-      The default value is 8.
-      .EXAMPLE
-      PS C:\> New-RandomComplexPassword 16
-  #>
-  param ( 
-    [parameter(
-        Mandatory=$false,
-        Position=0
-    )]
-    [int]$Length = 8
-  )
-  
-  $Assembly = Add-Type -AssemblyName System.Web
-  $RandomComplexPassword = [System.Web.Security.Membership]::GeneratePassword($Length,2)
-  return $RandomComplexPassword
+    param ( [int]$Length = 8 )
+    try {
+        $Assembly = Add-Type -AssemblyName System.Web
+        $RandomComplexPassword = [System.Web.Security.Membership]::GeneratePassword($Length,2)
+        Write-Output $RandomComplexPassword
+    }
+    catch
+    {
+        Write-Error $_
+    }
 }
